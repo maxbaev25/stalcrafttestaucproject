4141d0b7c2a09e2d157f6d320a59544b4bfaf023
MaxBaev <mbaev2012@mail.ru>
2026-03-24 18:31:44 +0300

    added ItemHistoryLot class to models.py crud is now on sqlite3: added new add_history_lot function

--- db/crud.py
+++ db/crud.py
@@ -1,12 +1,17 @@
-import datetime
-from sqlalchemy.orm import Session
-from db.engine import engine
-from db.models import ItemHistoryLot
+from datetime import datetime
+import sqlite3
 
 def add_history_lot(
-        amount: int, time: datetime.datetime,
-        price: int) -> None:
-    with Session(bind=engine) as session:
-        session.add(ItemHistoryLot(
-            amount=amount, time=time, price=price))
-        session.commit()
+        amount: int, time: datetime, price: int) -> None:
+    try:
+        connection = sqlite3.connect("stalcraft.db")
+        cursor = connection.cursor()
+        cursor.execute(
+            f'''INSERT INTO ItemHistoryLots 
+                   (price, amount, time) 
+               VALUES ({price}, {amount}, '{time}')'''
+        )
+        connection.commit()
+        connection.close()
+    except Exception as e:
+        print(e)
--- db/models.py
+++ db/models.py
@@ -1,16 +1,8 @@
 from datetime import datetime
 
-from sqlalchemy import Column, Integer, String, \
-    BOOLEAN, Text, DateTime
-from sqlalchemy.orm import DeclarativeBase
 
-
-class Base(DeclarativeBase):
-    pass
-
-class ItemHistoryLot(Base):
-    __tablename__ = 'item_history_lots'
-    id = Column(Integer, primary_key=True)
-    price = Column(Integer)
-    amount = Column(Integer)
-    time = Column(DateTime)
+class ItemHistoryLot:
+    id: int
+    amount: int
+    time: datetime
+    price: int
