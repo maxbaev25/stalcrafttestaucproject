7e3b627b87b663b390a0fb263d8ee8307ff02594
Bulbra <darthartem76@gmail.com>
2026-03-28 19:58:00 +0300

    Update add_history_lot to aync
    async function

--- db/crud.py
+++ db/crud.py
@@ -3,10 +3,10 @@ from sqlalchemy.orm import Session
 from db.engine import engine
 from db.models import ItemHistoryLot
 
-def add_history_lot(
-        amount: int, time: datetime.datetime,
+async def add_history_lot(
+        amount: int, time: str,
         price: int) -> None:
     with Session(bind=engine) as session:
-        session.add(ItemHistoryLot(
+        session.add(StalcraftHistoryTable(
             amount=amount, time=time, price=price))
         session.commit()
